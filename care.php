210d6d109c49159ddc5e9d3e3d6c781c56017850
Manu Lanikeu <emmanuel.arethens1@gmail.com>
2017-06-13 14:09:17 +0200

    exercice carré

---
+++ care.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of care
+ *
+ * @author arethens
+ */
+class care {
+    private $taille;
+    private $couleur;
+    
+    function __construct($taille, $couleur) {
+        $this->taille = $taille;
+        $this->couleur = $couleur;
+    }
+    function getTaille() {
+        return $this->taille;
+    }
+
+    function getCouleur() {
+        return $this->couleur;
+    }
+
+    function setTaille($taille) {
+        $this->taille = $taille;
+    }
+
+    function setCouleur($couleur) {
+        $this->couleur = $couleur;
+    }
+public function superficie(){
+    echo $this->taille*$this->taille.'px^2';
+    
+}
+
+  public function afficher():string {
+        $style = 'width:' . $this->taille . 'px;'
+                . 'height:' . $this->taille . 'px;'
+                . 'background-color:' . $this->couleur;
+        
+          return '<div style="' . $style . '"></div>'; }
+
+}
---
+++ formulairepersonne.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        ?>
+        
+        <form action="" method="post">
+        <label>Nom :</label>
+        <input type="text" name="nom"/>
+        <label>Prenom :</label>
+        <input type="text" name="prenom"/>
+        <label>Age :</label>
+        <input type="text" name="age"/>
+        <label>Région :</label>
+        <input type="text" name="region"/>
+        </form>
+    </body>
+</html>
